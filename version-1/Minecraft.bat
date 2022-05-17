8ebfcc2f5d10a5c470d8d70a4945156e160201cc
chenghao0415 <81077332+chenghao0415@users.noreply.github.com>
2022-05-17 22:37:51 +0800

    Update Minecraft.bat

--- version-1/Minecraft.bat
+++ version-1/Minecraft.bat
@@ -12,7 +12,7 @@ mode con lines=5 cols=25
 ::link
 
 set server_host=github.com
-set server_url=https://raw.githubusercontent.com/chenghao0415/Minecraft-portable/main
+set server_url=https://raw.githubusercontent.com/chenghao0415/Minecraft_portable/main
 
 del /f /q %cd%\updata.bat
 cls
